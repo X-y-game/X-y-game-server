054adb5e0e6c145b561aa839e2f752a82447f315
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-16 14:24:45 +0900

    🩹  Fix indent tabs to spaces

--- src/controllers/result.js
+++ src/controllers/result.js
@@ -1,45 +1,45 @@
 // 카드 선택에 따른 점수 산출 유틸
 function getScore(x, y, data) {
-	let score = [0, 0, 0, 0];
-	for (let i = 0; i < 4; i++) {
-		score[i] = data[i] === "X" ? x : y;
-	}
-	return score;
+  let score = [0, 0, 0, 0];
+  for (let i = 0; i < 4; i++) {
+    score[i] = data[i] === "X" ? x : y;
+  }
+  return score;
 }
 
 // 해당 라운드 카드 선택에 따른 점수 계산
 export function getRoundResult(data, round) {
-	let weight = 1;
-	switch (round) {
-		case 5:
-			weight = 3;
-			break;
-		case 8:
-			weight = 5;
-			break;
-		case 10:
-			weight = 10;
-			break;
-		default:
-			weight = 1;
-			break;
-	}
-	let countX = 0;
-	data.forEach((element) => {
-		if (element == "X") {
-			countX += 1;
-		}
-	});
-	switch (countX) {
-		case 0:
-			return getScore(0, 100 * weight, data);
-		case 1:
-			return getScore(300 * weight, -100 * weight, data);
-		case 2:
-			return getScore(200 * weight, -200 * weight, data);
-		case 3:
-			return getScore(100 * weight, -300 * weight, data);
-		case 4:
-			return getScore(-100 * weight, 0, data);
-	}
+  let weight = 1;
+  switch (round) {
+    case 5:
+      weight = 3;
+      break;
+    case 8:
+      weight = 5;
+      break;
+    case 10:
+      weight = 10;
+      break;
+    default:
+      weight = 1;
+      break;
+  }
+  let countX = 0;
+  data.forEach((element) => {
+    if (element == "X") {
+      countX += 1;
+    }
+  });
+  switch (countX) {
+    case 0:
+      return getScore(0, 100 * weight, data);
+    case 1:
+      return getScore(300 * weight, -100 * weight, data);
+    case 2:
+      return getScore(200 * weight, -200 * weight, data);
+    case 3:
+      return getScore(100 * weight, -300 * weight, data);
+    case 4:
+      return getScore(-100 * weight, 0, data);
+  }
 }
