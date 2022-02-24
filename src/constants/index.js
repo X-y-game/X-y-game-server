11507a00b2db367cf51437d840205c1007181070
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-24 18:36:37 +0900

    chore: Update client endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const CLIENT_ENDPOINT = "https://x-y-game.github.io/x-y-game";
+export const CLIENT_ENDPOINT = "https://x-y-game.github.io";
 export const MAX_ROUND = 10;
 export const MAX_TEAM = 4;
